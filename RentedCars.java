112cb9a3272c06d21ab9796ddee37026045875d3
Dragos <dragosg094@gmail.com>
2020-06-04 22:32:29 +0300

    first commit

---
+++ RentedCars.java
@@ -0,0 +1,27 @@
+package HashMap;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class RentedCars extends CarRentalSystem{
+
+
+    private ArrayList<String> rentedCars;
+
+
+
+    public RentedCars(){
+        rentedCars = new ArrayList<>();
+    }
+
+
+    public ArrayList<String> getRentedCars() {
+
+        return rentedCars;
+    }
+    public int getSize(){
+
+        return getRentedCars().size();
+    }
+
+}
